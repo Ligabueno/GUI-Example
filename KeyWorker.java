a4533c4072f6cd784a164f1832499cbc7f7cc2ae
Ligabueno <79193595+Ligabueno@users.noreply.github.com>
2023-05-26 14:12:52 +0200

    Add files via upload

---
+++ KeyWorker.java
@@ -0,0 +1,20 @@
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+/**
+ *
+ * Description
+ *
+ * @version 1.0 from 26.05.2023
+ * @author 
+ */
+
+public class KeyWorker extends KeyAdapter {
+  
+  public void keyPressed(KeyEvent e) {
+    super.keyPressed(e);
+    if (e.getKeyCode == KeyEvent.VK_W) {
+      
+    } // end of if
+  }
+  } 
--- Panel.java
+++ Panel.java
@@ -3,14 +3,13 @@ import java.awt.*;
 import java.awt.Dimension;
 
 public class Panel extends JPanel{
-    
-
-    public Panel(Dimension d ){
-        setBounds(0 ,0, d.width,d.height);
-        setVisible(false);
-        setBackground(Color.BLACK);
-        setOpaque(true);
-    }
-
-
+       
+  
+  public Panel(Dimension d ){
+    setBounds(0 ,0, d.width,d.height);
+    setVisible(false);
+    setBackground(Color.BLACK);
+    setOpaque(true);
+    addKeyListener(new KeyWorker());
+  }
 }
